5df2a0ee54878d83e998b65739b5a8d483d549db
CODe1995 <code.57x53@gmail.com>
2021-09-26 23:25:11 +0900

    feat: circle reader를 구현한다
    - circle을 읽어와 원하는 경로에 그래프화 가능한 json 파일로 생성한다

---
+++ src/CircleReader/util/init-builtin-options.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+exports.initBuiltInOperator = void 0;
+var builtin_operator_1 = require("../circle-analysis/circle/builtin-operator");
+function initBuiltInOperator(model) {
+    var builtinCodes = [];
+    for (var opcode_idx = 0; opcode_idx < model.operatorCodesLength(); opcode_idx++) {
+        builtinCodes[opcode_idx] = builtin_operator_1.BuiltinOperator[model.operatorCodes(opcode_idx).builtinCode()];
+    }
+    return builtinCodes;
+}
+exports.initBuiltInOperator = initBuiltInOperator;
